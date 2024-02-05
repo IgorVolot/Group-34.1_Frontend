cc640b4bec74376c6a7443c50e7af07f177f4756
Igor Volotovskyi <ihor737@gmail.com>
2024-02-05 12:55:57 +0100

    Lesson 17 added. HW 17 added.

---
+++ Lessons/17-js-advanced3/destruct.js
@@ -0,0 +1,37 @@
+console.log('=========  Destructuring of array  ========');
+
+const arr = [2, 3, 5, 7, 11];
+let [a, b, ...c] = arr;
+console.log(a, b);
+console.log(c);
+
+console.log('============ swap ==========');
+let x = 10;
+let y = 15;
+console.log(x, y);
+[y, x] = [x, y];
+console.log(x, y);
+
+console.log('=========  Destructuring of object  ========');
+const person = {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 34,
+    fullName: function () {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    'my hobby': 'chess'
+}
+
+let {firstName: fName, 'my hobby': hobby, lastName, ...rest} = person;
+console.log(fName);
+console.log(lastName);
+console.log(rest);
+personInfo(person);
+console.log(person['my hobby']);
+console.log(hobby);
+
+function personInfo({lastName, age}) {  // let {lastName, age} = person;
+    console.log(`${lastName} - ${age}`);
+}
+
