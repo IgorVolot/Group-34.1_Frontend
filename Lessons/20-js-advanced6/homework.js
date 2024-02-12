bd19b7420bff9086fdf8024d7335cb6b59783d9e
Igor Volotovskyi <ihor737@gmail.com>
2024-02-12 10:06:28 +0100

    Lesson 20 updated.

--- Lessons/20-js-advanced6/homework.js
+++ Lessons/20-js-advanced6/homework.js
@@ -5,27 +5,60 @@
 //     }, 1000);
 // }
 
-// Solution 1 -> use let
+// // Solution 1 -> use let
+// for (let i = 0; i < 10; i++) {
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 1000);
+// }
+//
+// // Solution 2 -> :( add a variable to loop and reassign the variable for output
+// for (var i = 0; i < 10; i++) {
+//     let j = i;
+//     setTimeout(function () {
+//         console.log(j);
+//     }, 1000);
+// }
+//
+// // Solution 3 -> use anonymous wrapper function
+// for (var i = 0; i < 10; i++) {
+//     (function(j) {
+//         setTimeout(function () {
+//             console.log(j);
+//         }, 1000);
+//     })(i);
+// }
+//
+// // Solution 4 -> use wrapper function
+// for (var i = 0; i < 10; i++) {
+//     function fn(j) {
+//         setTimeout(function () {
+//             console.log(j);
+//         }, 1000);
+//     }
+//     fn(i);
+// }
+//
+// // Solution 5 -> use setTimeout arguments
+// for (let i = 0; i < 10; i++) {
+//     setTimeout(function (i) {
+//         console.log(i);
+//     }, 1000, i);
+// }
+
+// // Solution 5.1 -> use setTimeout arguments
+// for (let i = 0; i < 10; i++) {
+//     setTimeout(console.log, 1000);
+// }
+
+// // Solution 6 -> use bind
 for (let i = 0; i < 10; i++) {
-    setTimeout(function () {
-        console.log(i);
-    }, 1000);
+    setTimeout(console.log.bind(console, i), 1000);
 }
 
-// Solution 2 -> add a variable to loop and reassign the variable for output
-for (var i = 0; i < 10; i++) {
-    let j = i;
-    setTimeout(function () {
-        console.log(j);
-    }, 1000);
-}
-
-// Solution 3 -> Each iteration of loop creates a function that takes the current value i as argument j
-for (var i = 0; i < 10; i++) {
-    (function(j) {
-        setTimeout(function () {
-            console.log(j);
-        }, 1000);
-    })(i);
-}
-
+// // Solution 6 -> :)
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(function () {
+//         console.log(i++ % 10);
+//     }, 1000);
+// }
