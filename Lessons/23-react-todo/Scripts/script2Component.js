00dbb76290698bdb4fc473a7f0a2564bc8a8bd2f
Igor Volotovskyi <ihor737@gmail.com>
2024-02-15 16:35:14 +0100

    Lessons 23, 24 updated.

---
+++ Lessons/23-react-todo/Scripts/script2Component.js
@@ -0,0 +1,7 @@
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+const Group = () => {
+    return <h1>Cochort 34.1</h1>
+}
+root.render(<Group />);
